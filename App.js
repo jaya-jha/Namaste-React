d00da59503e01c75e15d52dec43dbdde85414d88
jaya kumari <jaya.kumari@mirumagency.com>
2023-01-07 02:34:00 +0530

    Third class code

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
-import React from "react"; 
-import  ReactDOM  from "react-dom/client";
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "Heading1",
-  },
-  "Heading1"
-);
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "Heading2",
-  },
-  "Heading2"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const Title = () => (
+<h1 id="title" key="h3">
+    Namaste React
+  </h1>
 );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
+
+  
+// javascript funtion which returns react element 
+// component composition
+const HeaderComponent = () =>
+   (
+     <div>
+       <Title/>
+       {Title()}  
+       {console.log("any js code")}  
+      <h1> Namaste React Funtional Component</h1>
+      <h2>heading 2</h2>
+    </div>
+  );
+
+ 
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing react element inside root
-root.render(container);
+root.render(<HeaderComponent />);
