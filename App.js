3f7fbd959836c2eff3414e2d1c15a77296fc122b
jaya kumari <jaya.kumari@mirumagency.com>
2023-01-09 20:27:26 +0530

    assignment for Third class

--- App.js
+++ App.js
@@ -1,31 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-
-const Title = () => (
-<h1 id="title" key="h3">
-    Namaste React
-  </h1>
-);
+import logo from "./assets/logo.jpg";
+import user from "./assets/user.png";
 
 
   
 // javascript funtion which returns react element 
 // component composition
-const HeaderComponent = () =>
-   (
-     <div>
-       <Title/>
-       {Title()}  
-       {console.log("any js code")}  
-      <h1> Namaste React Funtional Component</h1>
-      <h2>heading 2</h2>
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+    <img src={logo} className="logo" alt="logo"/>
+    <input className="searchBox" type="text" placeholder="Search"/>
+    <img src={user} className="user" alt="user"/>
     </div>
-  );
+ );
+};
+
 
  
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing react element inside root
-root.render(<HeaderComponent />);
+root.render(<HeaderComponent/>);
